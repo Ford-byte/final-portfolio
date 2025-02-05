a7d75f83b6012b12f17ad50bba46f54fd940c933
Ford-byte <c.iyac123@gmail.com>
2025-02-05 10:14:01 +0800

    chore: trial

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 "use client";
-import dynamic from "next/dynamic";
-
-const Banner = dynamic(() => import("./blocks/Banner.jsx"), { ssr: false });
-const About = dynamic(() => import("./blocks/About.jsx"), { ssr: false });
-const Skills = dynamic(() => import("./blocks/Skills.jsx"), { ssr: false });
-const Projects = dynamic(() => import("./blocks/Projects.jsx"), { ssr: false });
-const Contact = dynamic(() => import("./blocks/Contact.jsx"), { ssr: false });
-const Footer = dynamic(() => import("./layout/footer.jsx"), { ssr: false });
+// import dynamic from "next/dynamic";
+import Banner from "./blocks/Banner";
+import Skills from "./blocks/Skills";
+import Contact from "./blocks/Contact";
+import Footer from "./layout/footer";
+import About from "./blocks/About";
+import Projects from "./blocks/Projects";
+// const Banner = dynamic(() => import("./blocks/Banner.jsx"), { ssr: false });
+// const About = dynamic(() => import("./blocks/About.jsx"), { ssr: false });
+// const Skills = dynamic(() => import("./blocks/Skills.jsx"), { ssr: false });
+// const Projects = dynamic(() => import("./blocks/Projects.jsx"), { ssr: false });
+// const Contact = dynamic(() => import("./blocks/Contact.jsx"), { ssr: false });
+// const Footer = dynamic(() => import("./layout/footer.jsx"), { ssr: false });
 export default function Home() {
   return (
     <>
